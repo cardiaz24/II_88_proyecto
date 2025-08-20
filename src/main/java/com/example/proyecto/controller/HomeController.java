ad4c96ec9fd76ff1ef15f9e732a95ce27aa1a305
Carlos Diaz Artavia <cardiaz24@hotmail.com>
2025-08-19 20:28:05 -0600

    pruebas home

--- src/main/java/com/example/proyecto/controller/HomeController.java
+++ src/main/java/com/example/proyecto/controller/HomeController.java
@@ -1,18 +1,13 @@
 package com.example.proyecto.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String homeRedirect(Authentication auth) {
-        if (auth != null && auth.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
-            return "redirect:/libros";
-        }
-        return "redirect:/prestamos/mis-libros";
+    @GetMapping( "/home")
+    public String home() {
+        return "home";
     }
 }
